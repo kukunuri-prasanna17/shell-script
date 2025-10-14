113f1bd59f63c4979d344b87e84ef3536fc0343c
srilakshmicloud <prasannakukunuri53@gmail.com>
2025-10-14 18:58:37 +0530

    gmail

--- disk-usage.sh
+++ disk-usage.sh
@@ -1,21 +1,3 @@
-#!/bin/bash
-TO_ADDRESS=$1
-SUBJECT=$2
-ALERT_TYPE=$3
-MESSAGE_BODY=$4
-IP_ADDRESS=$5
-TO_TEAM=$6
-FORMATTED_BODY=$(printf '$s/n' "$MESSAGE_BODY" | sed -e  's/[]\/$*.^[]/\\&/g')
-
-FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/iP-ADDRESS/$IP_ADDRESS/g" -e "s/MESSAGE/$FORMATTED_BODY/g" template.html)
-
-{
-echo "To: $TO_ADDRESS"
-echo "Subject: $SUBJECT"
-echo "Content-Type: text/html"
-echo ""
-echo "$FINAL_BODY"
-} | msmtp "$TO_ADDRESS"
 DISK_USAGE=$(df -hT | grep -v filesystem)
 DISK_THRESHOLD=2
 IP_ADDRESS= curl -s http://169.254.169.254/latest/meta-data/public-ipv4
