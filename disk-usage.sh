ac1e3687cc404b3840ab18245d55a38d0694c6e4
srilakshmicloud <prasannakukunuri53@gmail.com>
2025-10-14 19:07:08 +0530

    gmail

--- disk-usage.sh
+++ disk-usage.sh
@@ -1,14 +1,14 @@
 DISK_USAGE=$(df -hT | grep -v filesystem)
 DISK_THRESHOLD=2
-IP_ADDRESS= curl -s http://169.254.169.254/latest/meta-data/public-ipv4
+IP_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
 MESSAGE=" "
 
-while IFS= read disk
+while IFS= read -r read disk
 do 
   usage=$(echo $disk df -hT | grep -v filesystem | awk '{print $6}' | cut -d "%" -f1)
   parition=$(echo $disk df -hT | grep -v filesystem | awk '{print $7}')
  if [ $usage -ge $DISK_THRESHOLD ]; then
-   MESSAGE+="High Usage alert on $parition:$usage % <br>"
+   MESSAGE+="High Usage alert on $parition: $usage % <br>"
  fi
 done <<< $DISK_USAGE
 
