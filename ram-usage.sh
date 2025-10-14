3b0925b933c266107565ea28e04da294f2d63804
srilakshmicloud <prasannakukunuri53@gmail.com>
2025-10-14 23:04:37 +0530

    gmail

--- ram-usage.sh
+++ ram-usage.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
+RAM_THRESHOLD=20000
+IP_ADDRESS=$(curl -s ifconfig.me)
+MESSAGE=" "
+
+while IFS= read -r line
+do
 RAM_USAGE=$(free | awk '{print $3}' | grep -v used | grep -v free)
 PARTITION=$(free | awk '{print $1}' | grep -v total)
-IP_ADDRESS=$(curl  http://169.254.169.254/latest/meta-data/public-ipv4)
-MESSAGE=" "
-MESSAGE="High Usage On $PARTITION: $RAM_USAGE"
-RAM_THRESHOLD=20000
-
 if [ $RAM_USAGE -ge $RAM_THRESHOLD ]; then
-   echo "Message body: $MESSAGE"
-sh gmail.sh "prasannakukunuri35@gmail.com" "High Alret Usage" "High Usage on RAM" "$MESSAGE" "$IP_ADDRESS" "DevOps Team,"
+   MESSAGE="High Usage On $PARTITION: $RAM_USAGE"
   else
    echo "RAM Storage is normal condition"
 fi
+done <<< $RAM_USAGE
+
+echo  "Message body: $MESSAGE"
+
+sh gmail.sh "prasannakukunuri35@gmail.com" "High Alret Usage" "High Usage on RAM" "$MESSAGE" "$IP_ADDRESS" "DevOps Team,"
 
 
 # TO_ADDRESS=$1
