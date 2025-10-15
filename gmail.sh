d4387369419e976d01bc05e0c9e1e091ec179cf9
srilakshmicloud <prasannakukunuri53@gmail.com>
2025-10-15 07:11:13 +0530

    gmail

--- gmail.sh
+++ gmail.sh
@@ -10,10 +10,18 @@ IP_ADDRESS=$5
 TO_TEAM=$6
 
 # Prepare final HTML email body from template
-FINAL_BODY=$(sed -e "s|TO_TEAM|$TO_TEAM|g" \
-                 -e "s|ALERT_TYPE|$ALERT_TYPE|g" \
-                 -e "s|IP-ADDRESS|$IP_ADDRESS|g" \
-                 -e "s|MESSAGE|$FORMATTED_BODY|g" template.html)
+TEMPLATE_PATH="/home/ec2-user/shell-script/template.html"
+
+if [ ! -f "$TEMPLATE_PATH" ]; then
+  echo "Error: HTML template not found at $TEMPLATE_PATH"
+  exit 1
+fi
+
+FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" \
+                 -e "s/ALERT_TYPE/$ALERT_TYPE/g" \
+                 -e "s/IP-ADDRESS/$IP_ADDRESS/g" \
+                 -e "s/MESSAGE/$FORMATTED_BODY/g" "$TEMPLATE_PATH")
+
 
 
 # Send email using msmtp
